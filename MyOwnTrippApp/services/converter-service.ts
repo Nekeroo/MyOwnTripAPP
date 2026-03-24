3d495e94ed1878ad4c1fdfef3d6dff576b5df965
EMGR35601 <mathieu.grattard-ext@francetravail.fr>
2026-03-24 11:02:26 +0100

    FEAT: Ajout appel pour la partie conversion

--- MyOwnTrippApp/services/converter-service.ts
+++ MyOwnTrippApp/services/converter-service.ts
@@ -1,5 +1,11 @@
 import axios from "axios";
 
+export interface ConvertRate {
+    amount: number;
+    base: string;
+    to: string;
+}
+
 export interface MoneyRate {
     base: string;
     date: string;
@@ -7,8 +13,16 @@ export interface MoneyRate {
 }
 const URL = "https://api.frankfurter.dev/v1"
 
+const LOCAL_URL = "http://localhost:3000"
+
 export async function retrieveLatestRates() {
     const response = await axios.get<MoneyRate>(URL+"/latest")
+
+    return response.data;
+}
+
+export async function convertRequestedRate(convertRate: ConvertRate) {
+    const response = await axios.post<ConvertRate>(LOCAL_URL+"/latest", convertRate)
 
     return response.data;
 }
